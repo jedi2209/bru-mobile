593dab733a7cd640541d31549c02e81b850af75a
Zavarka Team <support@zavarka.team>
2023-05-25 02:55:25 +0300

    Update version script

--- version.sh
+++ version.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash -e
 
-PROJECT_DIR="ios/bruapp"
-# ONESIGNAL_DIR="ios/OneSignalNotificationServiceExtension"
-INFOPLIST_FILE="Info.plist"
-INFOPLIST_DIR="${PROJECT_DIR}/${INFOPLIST_FILE}"
-# INFOPLIST_ONESIGNAL_DIR="${ONESIGNAL_DIR}/${INFOPLIST_FILE}"
+# Set the Xcode project file path
+APP_NAME="bruapp"
+PROJECT_FILE="./ios/$APP_NAME.xcodeproj/project.pbxproj"
 ANDROID_VERSION_FILE="android/app/app_version.properties"
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-
-# BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_DIR}")
 BUILD_NUMBER="0"
 
-
-## Update plist with new values
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PACKAGE_VERSION#*v}" "${INFOPLIST_DIR}"
-# /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "${INFOPLIST_DIR}"
-
-# /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PACKAGE_VERSION#*v}" "${INFOPLIST_ONESIGNAL_DIR}"
-# /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "${INFOPLIST_ONESIGNAL_DIR}"
-
+sed -i "" "s/MARKETING_VERSION = .*/MARKETING_VERSION = $PACKAGE_VERSION;/g" "$PROJECT_FILE"
+sed -i "" "s/CURRENT_PROJECT_VERSION = .*/CURRENT_PROJECT_VERSION = $BUILD_NUMBER;/g" "$PROJECT_FILE"
 
 ## Set Android version to 0
-echo "versionCode=0" > $ANDROID_VERSION_FILE
+echo "versionCode=${BUILD_NUMBER}" > $ANDROID_VERSION_FILE
 
-# git add "${INFOPLIST_DIR}" "${INFOPLIST_ONESIGNAL_DIR}" $ANDROID_VERSION_FILE
-git add "${INFOPLIST_DIR}" $ANDROID_VERSION_FILE
+git add "${PROJECT_FILE}" $ANDROID_VERSION_FILE
