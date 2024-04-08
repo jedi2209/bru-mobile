e11b524722b127734f5cb468be8cedea080f116e
Vitalii Dubanovskyi <vitaliydubik.work@gmail.com>
2024-04-08 12:08:50 +0300

    removed logs

--- src/helpers/hasTranslation.js
+++ src/helpers/hasTranslation.js
@@ -1,13 +1,5 @@
 const languages = ['en_US', 'de_US'];
 
 export const hasTranslation = lang => {
-  console.log(lang);
-  return languages.some(item => {
-    console.log(item, lang);
-    if (item === lang) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  return languages.some(item => item === lang);
 };
