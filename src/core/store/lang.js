eed04affeecb9ba068e7c3b57451432ffad0ea65
Vitalii Dubanovskyi <vitaliydubik.work@gmail.com>
2024-04-09 16:43:11 +0300

    language store fix

--- src/core/store/lang.js
+++ src/core/store/lang.js
@@ -8,59 +8,37 @@ import {hasTranslation} from '../../helpers/hasTranslation';
 
 const storeName = 'appLang';
 
-export const setLanguage = createEvent();
-export const initLanguage = createEvent();
-const reset = createEvent();
-
 const deviceLanguage =
   Platform.OS === 'ios'
     ? NativeModules.SettingsManager.settings.AppleLocale ||
       NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
     : NativeModules.I18nManager.localeIdentifier;
 
+export const setLanguage = createEffect(async lang => {
+  await AsyncStorage.setItem(storeName, lang);
+  i18n.changeLanguage(lang);
+});
+export const initLanguage = createEffect(async () => {
+  const savedLang = await AsyncStorage.getItem(storeName);
+  if (hasTranslation(deviceLanguage)) {
+    i18n.changeLanguage(savedLang || deviceLanguage);
+    return savedLang || deviceLanguage;
+  } else {
+    i18n.changeLanguage('en_US');
+    return 'en_US';
+  }
+});
+const reset = createEvent();
+
 export const $langSettingsStore = createStore(LANGUAGE.default.code, {
   name: storeName,
 })
+  .on(initLanguage.doneData, (_, lang) => lang)
   .on(setLanguage, (_, lang) => {
     i18n.changeLanguage(lang);
+    AsyncStorage.setItem('appLang', lang);
     return lang;
   })
-  .on(initLanguage, (_, lang) => lang)
   .reset(reset);
 
-const fetchLang = createEffect({
-  async handler() {
-    if (hasTranslation(deviceLanguage)) {
-      i18n.changeLanguage(deviceLanguage);
-      return deviceLanguage;
-    } else {
-      i18n.changeLanguage('en_US');
-      return 'en_US';
-    }
-  },
-});
-
-fetchLang.doneData.watch(result => {
-  initLanguage(result);
-});
-
-export const updateLang = createEffect({
-  async handler(lang) {
-    try {
-      await AsyncStorage.setItem(storeName, lang, err => {
-        if (err) {
-          console.error(err);
-        }
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  },
-});
-
-forward({
-  from: $langSettingsStore,
-  to: updateLang,
-});
-
-fetchLang();
+initLanguage();
